fdf96251aa1a5fd03010c213b469e9dacabdf241
Meareg Teferi <mearegbuna@gmail.com>
2025-05-28 10:16:12 -0700

    a header with styling to contain all the navbar definition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,10 @@ function Navbar() {
 
   return (
     <>
-      <div className="mb-96 h-20 pb-6 fixed top-0 right-0 left-0 z-50 flex justify-between shrink font-bold font-inter text-gray-300 bg-[#091b29]">
-        <div className="mt-6 ml-24 mr-36">logo</div>
-        <div className="flex justify-between gap-16  mr-20">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-[#091b29] text-gray-300 h-20 shadow-md">
+      <div className="max-w-7xl mx-auto px-6 flex items-center justify-between h-full">
+        {/* Logo */}
+        <div className="text-xl font-bold font-inter text-[#00c4cc]">Logo</div>
           <a href="#home"
             className={activeSection == "home" 
                 ? "py-2 px-3 mt-4 border-[#0191ca] border-[1px] rounded-lg mr-2 font-robotoSlab text-[#0191ca] hover:bg-[#0191ca] hover:text-black transition-all duration-300 ease-in-out"
